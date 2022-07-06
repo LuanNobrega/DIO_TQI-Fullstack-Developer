ea5cc306265499d159c0cf8f453bab8284f91289
Luan Nobrega <luan.snobrega@gmail.com>
2022-07-05 21:03:33 -0300

    segundo commit

---
+++ Módulo III - Javascript/assets/js/script_contador.js
@@ -0,0 +1,15 @@
+var numberwrapper = document.getElementById("number");
+var number = 0;
+
+function increment() {
+    number = number + 1;
+    numberwrapper.innerHTML = number;
+}
+
+function decrement() {
+    number = number - 1;
+    numberwrapper.innerHTML = number;
+}
+
+    
+
